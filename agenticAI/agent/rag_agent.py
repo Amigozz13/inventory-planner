992d2c7b759af557991af9ea4ce9feb6caa98ee8
adithya77777 <adithyagk1325@gmail.com>
2026-07-01 21:15:04 +0530

    Remove temporary VS Code files speciales

--- agenticAI/agent/rag_agent.py
+++ agenticAI/agent/rag_agent.py
@@ -1,28 +1,23 @@
 from state import State
+from  rag_policy.search import search_policy
+import os
+import sys
+project_root = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "..")
+)
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
 
-def rag_agent(state: State):
-
-    if state.get("error"):
+def rag_agent(state:State):
+    if state.get('error'):
         return state
-
-    inventory = state.get("inventory", {})
-
-    product = inventory.get("product_name", "Unknown Product")
-
-    policy = (
-        f"Maintain safety stock for {product}. "
-        f"Reorder before inventory reaches the average demand during lead time."
-    )
-
-    # Store global policy
-    state["policy"] = policy
-
-    # Store in latest inventory
-    inventory["policy"] = policy
-
-    # Store in every historical record
-    if state.get("all_dates_inventory"):
-        for item in state["all_dates_inventory"]:
-            item["policy"] = policy
-
+    inventory=state.get('inventory',{})
+    product=inventory.get('product_name','unknown product')
+    risk=state.get('risk','low')
+    query=f"what is the comany policy for {product} when inventory risk is {risk}"
+    policy=search_policy(query)
+    state['policy']=policy
+    if state.get('all_dates_inventory'):
+        for items in state['all_dates_inventory']:
+            items['policy']=policy
     return state
--- agenticAI/graph.py
+++ agenticAI/graph.py
@@ -1,39 +1,26 @@
 from state import State
 from langgraph.graph import StateGraph, START, END
-
 from agent.inventory_agent import inventory_agent
 from agent.demand_agent import demand_agent
 from agent.risk_agent import risk_analysis
 from agent.rag_agent import rag_agent
 from agent.recommendation import recommentated_agent
-
 builder = StateGraph(State)
-
-# Nodes
 builder.add_node("inventory", inventory_agent)
 builder.add_node("demand", demand_agent)
 builder.add_node("risk", risk_analysis)
 builder.add_node("rag", rag_agent)
 builder.add_node("recommendation", recommentated_agent)
-
-# Flow
 builder.add_edge(START, "inventory")
 builder.add_edge("inventory", "demand")
 builder.add_edge("demand", "risk")
-
-# Conditional Routing
 def route(state: State):
-
     if state.get("error"):
         return "recommendation"
-
     risk = state.get("risk", "Low").lower()
-
     if risk in ["high", "medium"]:
         return "rag"
-
     return "recommendation"
-
 builder.add_conditional_edges(
     "risk",
     route,
@@ -42,8 +29,6 @@ builder.add_conditional_edges(
         "recommendation": "recommendation"
     }
 )
-
 builder.add_edge("rag", "recommendation")
 builder.add_edge("recommendation", END)
-
 graph = builder.compile()
